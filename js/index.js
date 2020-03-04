9a409f5ca8feed16f3e23decd4fc09588587ad3e
Anton Ilchenko <anton.wmt@gmail.com>
2020-03-04 19:32:37 +0100

    Small fixes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import {Draw} from './draw.js';
+import { Draw } from './draw.js';
 
 /** Function that maps varible within given range. */
 const xmap = (n, start1, stop1, start2, stop2) => {
@@ -11,28 +11,28 @@ const drawMandelbrot = (zoomFactorX, zoomFactorY) => {
     for (let j = 0; j < draw.h; j++) {
       let a = xmap(i, 0, draw.w, -zoomFactorX, zoomFactorX);
       let b = xmap(j, 0, draw.h, -zoomFactorY, zoomFactorY);
-  
+
       let ca = a;
       let cb = b;
       let n = 0;
-  
+
       while (n < maxi) {
         let aa = a * a - b * b;
         let bb = 2 * a * b;
         a = aa + ca;
         b = bb + cb;
-  
+
         if (a + b > maxz) {
           break;
         }
         n++;
       }
-  
-      let bright = xmap(n, 0 ,maxi, 0, 255);
+
+      let bright = xmap(n, 0, maxi, 0, 255);
       if (n === maxi) {
         bright = 0;
       }
-  
+
       draw.color = `rgb(${bright}, ${bright}, 0)`;
       draw.pset(i, j);
     }
